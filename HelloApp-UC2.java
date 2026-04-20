a74d8f7cf48f581715a7370c5146728e5916f7f1
tulikasharad <tulikasharad15@gmail.com>
2026-04-20 09:21:25 +0530

    Added UC files

---
+++ HelloApp-UC2.java
@@ -0,0 +1,11 @@
+/*
+*@author:Tulika Sharad
+*@version:1.0
+*/
+
+public class HelloApp2{
+ public static void main(String[] args){
+    String name = args[0];
+	System.out.println("Hello, " + name + "!");
+	}
+}
